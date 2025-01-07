ba6d3d2220ea1fcb4ab4eb97da1323abef1694e2
suryaprakash737 <suryaprakashu55@gmail.com>
2025-01-07 11:51:00 -0500

    yes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  output: "standalone", // ✅ Important for Netlify deployment
 };
 
 export default nextConfig;
