6cb5c25d21fe6d1114934a4831a9df7dae1442ea
suryaprakash737 <suryaprakashu55@gmail.com>
2025-03-06 18:33:51 -0500

    yes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  
   output: "standalone", // ✅ Required for Netlify Deployment
   eslint: {
     ignoreDuringBuilds: true, // ✅ Fix: Ignore ESLint errors in Netlify builds
